c4583a3b63a56e4f3f8b82bcc0e331fa7470eeeb
Pepe Valdivia <jvaldivia@softweb.pe>
2016-11-18 09:50:34 -0500

    El listado de menu de items retorna un json subtitulos con items[].

--- lib/MojoApp/Controller/Item.pm
+++ lib/MojoApp/Controller/Item.pm
@@ -3,13 +3,44 @@ use Mojo::Base 'Mojolicious::Controller';
 use MojoApp::Model::Items;
 use JSON;
 use Data::Dumper;
+use strict;
+use warnings;
 
 sub menu {
   	my $self = shift;
   	my $nombreModulo = $self->param('nombreModulo');
     my $model = 'MojoApp::Model::Items';
     my $items= $model->new();
-    my @rpta = $items->menu($nombreModulo);
+    my @items = $items->menu($nombreModulo);
+
+    my @subtitulos_temp;
+    my %items_temp;
+
+    for my $item (@items) {
+		if($item->{"subtitulo"} ~~ @subtitulos_temp){ 
+			my %temp_item = ( item => $item->{"item"}, subtitulo => $item->{"url"});
+			#print("\n");print Dumper(\%temp_item);print("\n");
+			my @temp = $items_temp{$item->{"subtitulo"}};
+			@temp = @temp[0];
+			push @temp[0], {%temp_item};
+			$items_temp{$item->{"subtitulo"}} = @temp[0];
+		}else{
+			push @subtitulos_temp, $item->{"subtitulo"};
+			my %temp_item = ( item => $item->{"item"}, subtitulo => $item->{"url"});
+			push(my @temp, { %temp_item } );
+			$items_temp{$item->{"subtitulo"}} = [@temp];
+		}
+	}
+
+	my @rpta;
+	for my $subtitulo(@subtitulos_temp){
+		my %temp;
+		$temp{"subtitulo"} = $subtitulo;
+		$temp{"items"} = $items_temp{$subtitulo};
+		#print("\n");print Dumper(%temp);print("\n");
+		push @rpta, {%temp};
+	}
+
     my $json_text = to_json \@rpta;
 
     $self->render(text => ("$json_text"));
